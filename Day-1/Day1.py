56df5cdbb172d7fb037a9927064913963d9aaf08
HASAN-CE <hasanrangooni308@gmail.com>
2024-12-08 21:48:37 +0530

    SIMPLE

--- Day-1/Day1.py
+++ Day-1/Day1.py
@@ -3,7 +3,7 @@ import pyjokes
 #here pyjokes is a module of the python which is installed with the help of the "Pip" Command
 joke = pyjokes.get_joke()
 print(joke)
-#Simple Python Code
+#Simple Python Code which is easily readable 
 print("HELLO THERE")
 a=10
-print("VALUE OF A IS: ",a)
+print("VALUE OF A IS: ", a)
