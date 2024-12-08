15787ddcf93bec50524024fb5c1fb9f02a482074
HASAN-CE <hasanrangooni308@gmail.com>
2024-12-08 21:56:27 +0530

    Indent

--- Day-1/Day1.py
+++ Day-1/Day1.py
@@ -1,8 +1,10 @@
 #Topic-Introduction to Python
 import pyjokes
+
 #here pyjokes is a module of the python which is installed with the help of the "Pip" Command
 joke = pyjokes.get_joke()
 print(joke)
+
 #Simple Python Code which is easily readable 
 print("HELLO THERE")
 a=10
