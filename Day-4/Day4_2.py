6256cf12375170aa91a07027037d74419ed19b8a
HASAN-CE <hasanrangooni308@gmail.com>
2024-12-09 12:20:37 +0530

    Tuple

--- Day-4/Day4_2.py
+++ Day-4/Day4_2.py
@@ -1,4 +1,17 @@
 #Tuple
 
-#A Tuple is an immutable data type in Python
+# A Tuple is an immutable data type in Python
 
+a = (1,2,3,4,5) #Tuple are enclosed in parentheses
+print(type(a))
+
+#empty tuple
+b = ()
+
+#tuple with one element
+c = (1,)
+print(type(c),c)
+
+#tuple with different data types
+d =  (1,6181,"PYTHON",True,None)
+print(d) 
---
+++ Day-4/Day4_3.py
@@ -0,0 +1,28 @@
+#Tuples Methods
+
+a = (38,"MANAV",18,True,None,58,98,56,85,58,25,35,58,"SHAH")
+print(a)
+
+#count() method 
+No = a.count(58)
+print(No)
+print(a)
+
+#index() method
+In = a.index(58)
+print(In) 
+
+#len() method
+print(len(a))
+
+#concatiton of a list of tuples
+#As Tuple are immutable it creates whole new tuple doenot change the existing tuple
+tup_1 = ("PYTHON","JS","RUBY","SHELL","RUST")
+tup_2 = (1,2,3,4)
+res = tup_1 + tup_2
+print(res) 
+
+#min()
+#max()
+#sum()
+#any()
