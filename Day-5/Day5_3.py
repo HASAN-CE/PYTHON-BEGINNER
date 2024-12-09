716bfb83008bb48a483b2e7e28a155eec5c1cd06
HASAN-CE <hasanrangooni308@gmail.com>
2024-12-09 23:14:00 +0530

    Set

--- Day-5/Day5_3.py
+++ Day-5/Day5_3.py
@@ -1,2 +1,22 @@
 #Set Methods
 s = set() 
+s = {"Python","Js",None,False,156,15,"html"}
+print(s,type(s))
+
+#1.add()
+s.add(566)
+print(s,type(s))
+
+#2.remove()
+s.remove(None)
+print(s,type(s))
+s.remove("Css")
+print(s,type(s))  #Raises an error  
+
+#3.copy()
+s.copy()
+print(s,type(s))
+
+#4.clear()
+s.clear()
+print(s,type(s))
