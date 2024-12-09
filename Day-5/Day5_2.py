2b5dc3f8f9c2f8525ef39a82c4cefb7699b80667
HASAN-CE <hasanrangooni308@gmail.com>
2024-12-09 23:04:37 +0530

    Set

--- Day-5/Day5_2.py
+++ Day-5/Day5_2.py
@@ -6,4 +6,10 @@ s = set()
 #in Set if repeat the element it will take only once 
 s1 = set()
 s1 = {1,2,4,8,10,12,14,16,18,20}
+print(s1)
 print(type(s1))
+
+s2 = set()
+s2 = {1,2,4,8,10,12,14,16,18,20,161,11,165,252,252,1,2,4,8,10,12,14,16}
+print(s2)
+print(type(s2))
