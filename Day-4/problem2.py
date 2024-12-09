92c9c89fcd032798727ca84a83f12d43b85381ce
HASAN-CE <hasanrangooni308@gmail.com>
2024-12-09 12:41:32 +0530

    Tuple

---
+++ Day-4/problem2.py
@@ -0,0 +1,22 @@
+marks = []
+
+M1 = int(input("ENTER MARKS FOR 1 STUDENT: "))
+marks.append(M1)
+
+M2 = int(input("ENTER MARKS FOR 2 STUDENT: "))
+marks.append(M2)
+
+M3 = int(input("ENTER MARKS FOR 3 STUDENT: "))
+marks.append(M3)
+
+M4 = int(input("ENTER MARKS FOR 4 STUDENT: "))
+marks.append(M4)
+
+M5 = int(input("ENTER MARKS FOR 5 STUDENT: "))
+marks.append(M5)
+
+M6 = int(input("ENTER MARKS FOR 6 STUDENT: "))
+marks.append(M6)
+
+marks.sort()
+print(marks)
