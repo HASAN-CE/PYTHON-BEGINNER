a9e4795b355c00d04a7a9baa11345e519c732bda
HASAN-CE <hasanrangooni308@gmail.com>
2024-12-09 20:56:06 +0530

    Dictionary

---
+++ Day-5/Day5_1.py
@@ -0,0 +1,22 @@
+#Dictionary Methods
+
+Dict = {
+    "Name" : "Python",
+    "Age" : 33,
+    "Owner" : "Guido van Rossum",
+    "Libraries" : ["NumPy","SciPy","TensorFlow"],
+    1991 :  "Birth_year",
+}
+
+# print(Dict)
+
+#Methods
+ 
+#1).items()
+print(Dict.items())
+
+#2).keys()
+print(Dict.keys())
+
+#3).values()
+print(Dict.values())
