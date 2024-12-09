a3a20b61103e442bcae1b682991630dd135f56c6
HASAN-CE <hasanrangooni308@gmail.com>
2024-12-09 23:01:52 +0530

    Set

--- Day-5/Day5_2.py
+++ Day-5/Day5_2.py
@@ -1,1 +1,9 @@
 #Sets
+#For creating empty set dont use like this e = {} as it will create a empty Dictionary
+s = set()
+
+#Set Example 
+#in Set if repeat the element it will take only once 
+s1 = set()
+s1 = {1,2,4,8,10,12,14,16,18,20}
+print(type(s1))
