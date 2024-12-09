17a7da1d878684343707b5e2e01c4ed662370ad8
HASAN-CE <hasanrangooni308@gmail.com>
2024-12-09 20:38:27 +0530

    Dictionary

--- Day-5/Day5_0.py
+++ Day-5/Day5_0.py
@@ -1,8 +1,10 @@
 #Dictionary
-
+#Dicionaries are like the  list of Key:Value Pairs
 marks = {
-    "Rohan": 56,
-    "Harish" : 98
+    "Rohan" : 56,
+    "Harish" : 98,
+    "Lists" : [1,2,3,45] 
 }
 print(marks)
 print(type(marks))
+print(marks["Lists"])
