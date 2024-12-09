140cb2881637b408edf0b86912cbf3bafe0e0b5d
HASAN-CE <hasanrangooni308@gmail.com>
2024-12-09 12:31:23 +0530

    Tuple

---
+++ Day-4/problem1.py
@@ -0,0 +1,24 @@
+fruits = []
+
+F1 = input("ENTER THE FRUITS 1 : ")
+fruits.append(F1)
+
+F2 = input("ENTER THE FRUITS 2 : ")
+fruits.append(F2)
+
+F3 = input("ENTER THE FRUITS 3 : ")
+fruits.append(F3)
+
+F4 = input("ENTER THE FRUITS 4 : ")
+fruits.append(F4)
+
+F5 = input("ENTER THE FRUITS 5 : ")
+fruits.append(F5)
+
+F6 = input("ENTER THE FRUITS 6 : ")
+fruits.append(F6)
+
+F7 = input("ENTER THE FRUITS 7: ")
+fruits.append(F7)
+
+print(fruits)
