a11458d1757b3e6ccdf0dc3fff09893b362b3ec0
HASAN-CE <hasanrangooni308@gmail.com>
2024-12-10 21:00:34 +0530

    Day-6

--- Day-6/Day6_1.py
+++ Day-6/Day6_1.py
@@ -1,10 +1,15 @@
 #Elif Example
 
-a = int(input('Enter the Number'))
+#If Elif Else ladder
+a = int(input('Enter the Number: '))
 
 if (a==0):
     print('Enter number is Zero')
+elif(a<0):
+    print('Enter number is Negative')
 elif(a%2==0):
     print('Enter number is Even')
 else:
     print('Enter number is Odd')
+    
+print('End of Elif Example')
