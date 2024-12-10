a15c8afba8121ebaa8ce66a388cac3aa5dba2b26
HASAN-CE <hasanrangooni308@gmail.com>
2024-12-10 20:41:13 +0530

    Day-6

---
+++ Day-6/Day6_0.py
@@ -0,0 +1,18 @@
+a = int(input("Enter your age: "))
+
+if (a>=18):
+    print("You are allowed to vote")
+else:
+    print("You are not allowed to vote") 
+    
+print("End of if-else statement")
+
+
+a = int(input("Enter the Number: "))
+
+if (a%2==0):
+    print(f"Number is even",a)
+else:
+    print(f"Number is odd",a) 
+    
+print("End of if-else statement")
