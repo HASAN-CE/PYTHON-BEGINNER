57bd1d929d9ac61f78ca620a4ac3f14e43ad4687
HASAN-CE <hasanrangooni308@gmail.com>
2024-12-11 13:13:18 +0530

    Loops

--- Day-7/Day7_2.py
+++ Day-7/Day7_2.py
@@ -2,6 +2,6 @@
 for i in range(4):
     print(i)
     
-# Range function in python is used to generate the sequence of number, it generates the numbers from 0 to 4 inclusive
+# Range function in python is used to generate the sequence of number, in this program it will  generates the numbers from 0 to 4 inclusive
 
 
