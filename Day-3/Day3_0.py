b9bf25b3bf2e40fab92352a145dd27bc86f00ef5
HASAN-CE <hasanrangooni308@gmail.com>
2024-12-13 20:59:01 +0530

    Day-3

--- Day-3/Day3_0.py
+++ Day-3/Day3_0.py
@@ -1,28 +1,31 @@
-#String 
-# String Are Immutable
+# String 
+# Strings in Python are immutable
 a = "PYTHON"
 b = '''PYTHON'''
-c =  'PYTHON'
+c = 'PYTHON'
 
-# name = len(a)   #len(var_name) give the length of the string 
-# print(name)
+# Get the length of the string 'a'
+name = len(a)  # len(var_name) gives the length of the string
+print(name)
 
-# name_Short = a[0:2]   #travel only 0-1 but not '2'
-# print(name_Short)
+# Extract characters from index 0 to 1 (excluding index 2)
+name_short = a[0:2]
+print(name_short)
 
+# Extract the fourth character from the end of the string 'c'
+short_form = c[-4]  # Output 'T'
+print(short_form)
 
-# short_form = c [-4]
-# print(short_form)
+# Extract the second character from the string 'b'
+short_form = b[1]  # Output 'Y'
+print(short_form)
 
-# short_form = b [1]
-# print(short_form)
+# Additional string manipulations
+ex = '''DECIMALL'''
+print(ex[:-1])  # Print all characters except the last one
+print(ex[0:])   # Print the entire string
 
-ex =  '''DECIMALL'''
-print(ex[:-1]) 
-print(ex[0:])
-
-
-#Skipping the Value
-d = ''''abcdefghijklmnopqrstuvwxyz'''
-print(d[2:12])  
-print(d[2:12:2])   #stars from 2 goes till 11 indices and skipping the indices with 2 spaces
+# Skipping values
+d = '''abcdefghijklmnopqrstuvwxyz'''
+print(d[2:12])  # Extract characters from index 2 to 11
+print(d[2:12:2])  # Start from index 2, go till index 11, and skip every second index
