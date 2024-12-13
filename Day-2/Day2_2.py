caff774ec17c9031424e6664fc5e98411c999156
HASAN-CE <hasanrangooni308@gmail.com>
2024-12-13 20:45:43 +0530

    Day-2

--- Day-2/Day2_2.py
+++ Day-2/Day2_2.py
@@ -1,4 +1,4 @@
-#Input Function input() it always returns the String even if the input is integer
+# The input() function in Python always returns the user input as a string, regardless of whether the entered value appears to be numeric or otherwise.
 a = int(input("ENTER NUMBER 1: "))
 print(a)
 b = int(input("ENTER NUMBER 2: "))
